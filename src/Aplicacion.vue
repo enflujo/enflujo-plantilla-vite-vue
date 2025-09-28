e081bd1eb11b9852c52ffa9afc34bfd70ac44ddc
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2025-09-28 08:49:12 -0500

    ⬆️ Actualizar dependencias + ejemplo SCSS

--- src/Aplicacion.vue
+++ src/Aplicacion.vue
@@ -1,13 +1,28 @@
 <script setup lang="ts">
 import Hola from './componentes/Hola.vue';
+const base = import.meta.env.BASE_URL;
 </script>
 
 <template>
-  <Hola />
+  <header>
+    <span class="icono"><img :src="`${base}favicon.svg`" alt="Logo EnFlujo" /></span>
+    <Hola />
+  </header>
 </template>
 
 <style lang="scss" scoped>
-#main {
-  background-color: yellow;
+header {
+  display: flex;
+
+  .icono {
+    width: 2em;
+    margin-right: 0.2em;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  }
 }
 </style>
